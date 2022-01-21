69f44a26b57352d84bf6b365532c5f11cde99a25
SiobhanRoche <siobhanroche@wit.ie>
2022-01-21 14:42:04 +0000

    Added New Activity

---
+++ app/src/main/java/org/wit/placemark/activities/GitHubActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.placemark.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.wit.placemark.R
+
+class GitHubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_hub2)
+    }
+}
